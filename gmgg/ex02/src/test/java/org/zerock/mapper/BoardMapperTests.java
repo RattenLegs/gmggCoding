7a2893e8412cd83ff04f45ce226623c613d16e3f
Rattanlegs <Rattanlegs@gmail.com>
2023-03-17 16:43:49 +0900

    CRUD까지 성공!

--- gmgg/ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ gmgg/ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -32,5 +32,41 @@ public class BoardMapperTests {
 		mapper.insert(board);
 		log.info(board);
 	}
+	
+	@Test
+	public void testInsertKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("newbie");
+		mapper.insertSelectKey(board);
+		log.info(board);
+	}
+	
+	@Test
+	public void testRead() {
+		//존재하는 게시물 번호로 리스트
+		BoardVO board = mapper.read(23L);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testDelete() {
+		log.info("DELECT COUNT: " + mapper.delete(23L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		
+		board.setBno(1L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user10");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
+	}
 
 }
